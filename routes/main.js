da91228d9a2ec9231eb28e6a0ac98318b2e7e384
Laura Sims <laurasimsdev@gmail.com>
2022-10-06 20:16:24 -0600

    Added About page

--- routes/main.js
+++ routes/main.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
+const aboutController = require("../controllers/about");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
+router.get("/about", aboutController.getAbout); //Added
 router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login", authController.getLogin);
